753dc1f0a8b9ba81656df162a4b1162f5437d086
Pablo <pablo.barbecho@upc.edu>
2021-05-27 22:16:01 +0200

    STGT GUI setup

--- stgt/__init__.py
+++ stgt/__init__.py
@@ -1,9 +1,9 @@
 from .stgt import DlgMain
-from .od2trips import od2
-from .duarouter import dua_ma
-from .duaiterate import duai
-from .randomTrips import rt
+#from .od2trips import od2
+#from .duarouter import dua_ma
+#from .duaiterate import duai
+#from .randomTrips import rt
 
-__all__ = ('od2', 'dua_ma', 'duai', 'rt')
+__all__ = ('DlgMain')
 
 name = 'stgt'
